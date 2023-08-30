d154d0ec0b395061cdaa9fe64e960c9121f04501
Nathaniel Aki <nathanielaki.na@gmail.com>
2023-08-30 18:33:09 +0100

    added serializers for models, views and urls to map api  endpoints

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,68 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.decorators import api_view
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from .models import Booking, Menu
+from .serializers import BookingSerializer, MenuSerializer, UserSerializer
 
 # Create your views here.
+
 def index(request):
     return render(request, 'index.html', {})
+
+# class bookingview(APIView):
+
+#     def get(self, request):
+#         items = Booking.objects.all()
+#         serializer = BookingSerializer(items, many=True)
+#         return Response(serializer.data)
+    
+#     def post(self, request):
+#         serializer = BookingSerializer(data=request.data)
+
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response({"status": "success", "data": serializer.data})
+
+# class menuview(APIView):
+
+#     def get(self, request):
+#         items = Menu.objects.all()
+#         serializer = MenuSerializer(items, many=True)
+#         return Response(serializer.data)
+    
+#     def post(self, request):
+#         serializer = MenuSerializer(data=request.data)
+
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response({"status": "success", "data": serializer.data})
+
+
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+from rest_framework.viewsets import ModelViewSet
+
+class BookingViewSet(ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+from django.contrib.auth.models import User
+from rest_framework import permissions
+
+class UserViewSet(ModelViewSet):
+   queryset = User.objects.all()
+   serializer_class = UserSerializer
+   permission_classes = [permissions.IsAuthenticated] 
+
